5c9d1cba55143e3cfb88c549a52f57423ccd4d5b
Jason Merriman <mrloureed@gmail.com>
2016-07-30 15:35:35 -0400

    high score added

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,12 +1,18 @@
 fc.scoreboard = (function() {
 	fc.score = 0;
+    fc.highscore = 0;
 
 	function update(o,update) {
 		o.html(update);
+        if (fc.score > fc.highscore) {
+            fc.highscore = fc.score;
+            fc.scoreboard.update(fc.$highscore, fc.highscore);
+        }
 	}
 
 	function init() {
 		fc.$score = $('[data-score]');
+        fc.$highscore = $('[data-highscore]');
 		fc.$level = $('[data-level]');
     }
 
